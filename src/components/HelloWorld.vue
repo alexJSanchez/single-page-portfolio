3ca0b05c8a7e060c0d5c83827d6ff2a8351d0470
alex sanchez <alexjsanchez1020@gmail.com>
2024-03-07 06:02:08 -0500

    add icons

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,12 +4,33 @@ const style = useStyleStore()
 </script>
 
 <template>
-  <div class="helloWorld"><p v-bind:style="style.typography.body">AlexSanchez</p></div>
+  <div class="helloWorld">
+    <p v-bind:style="style.typography.body">AlexSanchez</p>
+    <ul class="imageIconStyle">
+      <li><img class="imageIcon" src="../assets/icon-linkedin.svg" /></li>
+      <li><img class="imageIcon" src="../assets/icon-frontend-mentor.svg" /></li>
+      <li><img class="imageIcon" src="../assets/icon-github.svg" /></li>
+      <li><img class="imageIcon" src="../assets/icon-twitter.svg" /></li>
+    </ul>
+  </div>
 </template>
 
 <style scoped>
+.imageIcon {
+  width: 20px;
+  height: 20px;
+}
+.imageIconStyle {
+  display: flex;
+  justify-content: center;
+  gap: 1rem;
+  margin-top: 1rem;
+  list-style: none;
+}
 .helloWorld {
   text-align: center;
+  font-family: 'spaceFontMed';
+  margin: 1rem;
 }
 h1 {
   font-weight: 500;
