0f0b6298d12ec13baec07d8c4694f48b11d2d2e3
alex sanchez <alexjsanchez1020@gmail.com>
2024-03-09 05:03:09 -0500

    adding stack

--- src/components/ExperienceSection.vue
+++ src/components/ExperienceSection.vue
@@ -3,27 +3,128 @@
   <div style="margin-top: 3rem">
     <div style="background-color: #4ee1a0; padding: 1px; margin: 1rem"></div>
     <ul style="text-align: center; list-style: none">
-      <li>
-        <div style="/* Background color */ padding: 10px">
-          <img src="../assets/icon-html.png" style="width: 25%" />
-        </div>
-        <h2 style="color: #d9d9d9">4 years</h2>
-      </li>
-      <li>
+      <li
+        style="
+          display: grid;
+          grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
+          grid-template-rows: 1fr 1fr;
+        "
+      >
         <div
-          style="display: flex; justify-content: center; margin-top: 1rem; flex-direction: column"
+          style="
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            grid-column-start: 2;
+            grid-column-end: 3;
+            grid-row-start: 1;
+          "
         >
-          <h2 style="font-size: 2rem; color: white">CSS</h2>
-          <p style="color: #d9d9d9">4 year experience</p>
+          <img
+            src="../assets/icon-html.png"
+            style="
+              width: 100%;
+              max-width: 100px; /* Adjust the max-width as needed */
+              border-radius: 10%;
+              box-shadow:
+                0 0 5px 10px rgba(255, 255, 255, 0.3),
+                inset 0 0 10px 10px rgba(255, 255, 255, 0.3); /* Multiple shadows including the background shadow */
+            "
+          />
         </div>
-      </li>
-      <li>
         <div
-          style="display: flex; justify-content: center; margin-top: 1rem; flex-direction: column"
+          style="
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            grid-column-start: 4;
+            grid-column-end: 5;
+            grid-row-start: 1;
+          "
         >
-          <h2 style="font-size: 2rem; color: white">JavaScript</h2>
-          <p style="color: #d9d9d9">4 year experience</p>
+          <img
+            src="../assets/icon-css.svg"
+            style="
+              width: 100%;
+              max-width: 100px; /* Adjust the max-width as needed */
+              border-radius: 10%;
+              box-shadow:
+                0 0 5px 10px rgba(255, 255, 255, 0.3),
+                inset 0 0 10px 10px rgba(255, 255, 255, 0.3); /* Multiple shadows including the background shadow */
+            "
+          />
         </div>
+        <div
+          style="
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            grid-column-start: 3;
+            grid-column-end: 4;
+            grid-row-start: 2;
+          "
+        >
+          <img
+            src="../assets/icon-javascript.svg"
+            style="
+              width: 100%;
+              max-width: 75px; /* Adjust the max-width as needed */
+              max-height: 100px;
+              border-radius: 10%;
+              box-shadow:
+                0 0 5px 10px rgba(255, 255, 255, 0.3),
+                inset 0 0 10px 10px rgba(255, 255, 255, 0.3); /* Multiple shadows including the background shadow */
+            "
+          />
+        </div>
+        <h2
+          style="
+            grid-column-start: 3;
+            grid-column-end: 4;
+            grid-row-start: 3;
+            margin-top: 20px;
+            font-size: 1.8rem;
+          "
+        >
+          4Years
+        </h2>
+      </li>
+      <li style="margin: 1rem">
+        <div
+          style="display: flex; position: relative; justify-content: center; align-items: center"
+        >
+          <img
+            src="../assets/mernStack.png"
+            style="
+              width: 100%;
+              max-width: 8rem; /* Adjust the max-width as needed */
+              max-height: 8rem;
+            "
+          />
+          <div
+            style="
+              width: 8rem;
+              height: 8rem;
+              position: absolute;
+              border-radius: 30%;
+              box-shadow:
+                0 0 2px 2px rgba(255, 255, 255, 0.3),
+                inset 0 0 20px 20px rgba(255, 255, 255, 0.3); /* Multiple shadows including the background shadow */
+              transform: rotate(45deg); /* Rotate the box shadow by 45 degrees */
+            "
+          ></div>
+        </div>
+        <h2
+          style="
+            grid-column-start: 3;
+            grid-column-end: 4;
+            grid-row-start: 3;
+            margin-top: 20px;
+            font-size: 1.8rem;
+          "
+        >
+          4Years
+        </h2>
       </li>
       <li>
         <div
