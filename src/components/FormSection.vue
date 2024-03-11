4e6c927faa17b2cc96c6f8378250fd087962672d
alex sanchez <alexjsanchez1020@gmail.com>
2024-03-11 05:01:14 -0400

    contact setup return email

--- src/components/FormSection.vue
+++ src/components/FormSection.vue
@@ -1,16 +1,3 @@
-<template>
-  <form ref="form" @submit.prevent="sendEmail">
-    <label>Name</label>
-    <input type="text" name="user_name" />
-    <label>Email</label>
-    <input type="email" name="user_email" />
-    <label>Message</label>
-    <textarea name="message"></textarea>
-    <label>Contact</label>
-    <input type="email" name="user_email" />
-    <input type="submit" value="Send" />
-  </form>
-</template>
 <script>
 import emailjs from '@emailjs/browser'
 
@@ -33,7 +20,25 @@ export default {
   }
 }
 </script>
-
+<template>
+  <form ref="form" @submit.prevent="sendEmail">
+    <label>Name</label>
+    <input type="text" name="user_name" />
+    <label>Email</label>
+    <input type="email" name="user_email" />
+    <label>Message</label>
+    <textarea name="message"></textarea>
+    <label>Number</label>
+    <input
+      id="phone"
+      name="contact_number"
+      type="tel"
+      maxlength="10"
+      placeholder="Enter your phone number"
+    />
+    <input type="submit" value="Send" />
+  </form>
+</template>
 <!--
   <div style="background-color: #ffffff; padding: 1px; margin: 1rem; opacity: 0.5"></div>
   <div style="display: flex; flex-direction: column; align-items: center; padding: 0 1rem">
