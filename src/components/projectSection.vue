945c93f53b8e573ab755a3a05d872dde2d61c173
alex sanchez <alexjsanchez1020@gmail.com>
2024-03-12 01:30:35 -0400

    add contact anchor

--- src/components/projectSection.vue
+++ src/components/projectSection.vue
@@ -12,19 +12,21 @@ export default {
   <div style="margin: 2.5rem 1rem; display: flex; justify-content: space-between">
     <h2 style="font-family: spaceFontBold; font-size: 2rem">Projects</h2>
     <div>
-      <button
+      <a
+        href="#contact"
         :style="{
           color: store.colors.orochimaru,
+          fontFamily: 'spaceFontBold',
           borderStyle: 'none',
           backgroundColor: 'transparent',
           paddingBottom: '0.3rem',
           borderBottom: 'solid 2px #4ee1a0',
           letterSpacing: '2px',
-          lineHeight: '26px'
+          lineHeight: '26px',
+          textDecoration: 'none'
         }"
-      >
-        CONTACT ME
-      </button>
+        >Contact Me
+      </a>
     </div>
   </div>
 </template>
