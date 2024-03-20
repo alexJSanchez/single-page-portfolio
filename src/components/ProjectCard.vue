121be32568ec9f6b5c0f18c0f5d4d78c32b51345
alex sanchez <alexjsanchez1020@gmail.com>
2024-03-20 10:35:54 -0400

    adding some styles to project section

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -54,6 +54,12 @@ export default {
     <p style="grid-column-start: 1; grid-row-start: 3; grid-column-end: 8; font-size: 1rem">
       HTML CSS JAVASCRIPT TAILWIND VITE
     </p>
+    <h5 style="grid-column-start: 1; grid-row-start: 4; grid-column-end: 8">
+      Skateboarder assistant app that
+      <span style="text-decoration: underline #4ee1a0"
+        >uses user geo location to find the closest skatable spot</span
+      >. Additional quality of life >features like weather, time, and trick of the day.
+    </h5>
     <button
       :style="{
         color: store.colors.orochimaru,
@@ -66,7 +72,7 @@ export default {
         letterSpacing: '2px',
         lineHeight: '26px',
         gridColumnStart: '1',
-        gridRowStart: '4',
+        gridRowStart: '5',
         gridColumnEnd: '3'
       }"
     >
@@ -86,7 +92,7 @@ export default {
         letterSpacing: '2px',
         lineHeight: '26px',
         gridColumnStart: '4',
-        gridRowStart: '4',
+        gridRowStart: '5',
         gridColumnEnd: '7'
       }"
     >
@@ -97,77 +103,7 @@ export default {
       >
     </button>
   </div>
-  <!--second project-->
-  <div
-    style="
-      display: grid;
-      grid-template-rows: 4;
-      grid-template-columns: 8;
-      padding: 0 1rem;
-      margin-bottom: 2rem;
-    "
-  >
-    <img
-      style="width: 100%; grid-column-start: 1; grid-column-end: 8"
-      src="../assets/image-asylum.PNG"
-    />
-    <p
-      style="
-        grid-column-start: 1;
-        grid-column-end: 8;
-        grid-row-start: 2;
-        font-size: 1.5rem;
-        line-height: 32px;
-        margin: 1rem 0;
-      "
-    >
-      Asylum Grant Tracker
-    </p>
-    <p style="grid-column-start: 1; grid-row-start: 3; grid-column-end: 8; font-size: 1rem">
-      HTML CSS JAVASCRIPT REACT REDUX
-    </p>
-    <button
-      :style="{
-        color: store.colors.orochimaru,
-        margin: '.5rem 0',
-        fontFamily: 'spaceFontMed',
-        borderStyle: 'none',
-        backgroundColor: 'transparent',
-        paddingBottom: '0.3rem',
-        borderBottom: 'solid 2px #4ee1a0',
-        letterSpacing: '2px',
-        lineHeight: '26px',
-        gridColumnStart: '1',
-        gridRowStart: '4',
-        gridColumnEnd: '3'
-      }"
-    >
-      View Project
-    </button>
-    <button
-      :style="{
-        color: store.colors.orochimaru,
-        margin: '.5rem 0',
-        fontFamily: 'spaceFontMed',
-        borderStyle: 'none',
-        backgroundColor: 'transparent',
-        paddingBottom: '0.3rem',
-        borderBottom: 'solid 2px #4ee1a0',
-        letterSpacing: '2px',
-        lineHeight: '26px',
-        gridColumnStart: '4',
-        gridRowStart: '4',
-        gridColumnEnd: '7'
-      }"
-    >
-      <a
-        style="text-decoration: none; color: white"
-        href="https://github.com/alexJSanchez/asylum-rg-fe-starter"
-        >View code</a
-      >
-    </button>
-  </div>
-  <!--third project-->
+  <!--Second project-->
   <div
     style="
       display: grid;
@@ -196,6 +132,11 @@ export default {
     <p style="grid-column-start: 1; grid-row-start: 3; grid-column-end: 8; font-size: 1rem">
       HTML CSS JAVASCRIPT REACT
     </p>
+    <h5 style="grid-column-start: 1; grid-row-start: 4; grid-column-end: 8">
+      A simple design for a
+      <span style="text-decoration: underline #4ee1a0">photographer blog page</span> built in react
+      with tailwind css
+    </h5>
     <button
       :style="{
         color: store.colors.orochimaru,
@@ -208,7 +149,7 @@ export default {
         letterSpacing: '2px',
         lineHeight: '26px',
         gridColumnStart: '1',
-        gridRowStart: '4',
+        gridRowStart: '5',
         gridColumnEnd: '3'
       }"
     >
@@ -228,13 +169,87 @@ export default {
         letterSpacing: '2px',
         lineHeight: '26px',
         gridColumnStart: '4',
-        gridRowStart: '4',
+        gridRowStart: '5',
         gridColumnEnd: '7'
       }"
     >
       <a
         style="text-decoration: none; color: white"
         href="https://github.com/alexJSanchez/photoblog"
+        >View code</a
+      >
+    </button>
+  </div>
+  <!--Third project-->
+  <div
+    style="
+      display: grid;
+      grid-template-rows: 4;
+      grid-template-columns: 8;
+      padding: 0 1rem;
+      margin-bottom: 2rem;
+    "
+  >
+    <img
+      style="width: 100%; grid-column-start: 1; grid-column-end: 8"
+      src="../assets/image-asylum.PNG"
+    />
+    <p
+      style="
+        grid-column-start: 1;
+        grid-column-end: 8;
+        grid-row-start: 2;
+        font-size: 1.5rem;
+        line-height: 32px;
+        margin: 1rem 0;
+      "
+    >
+      Asylum Grant Tracker
+    </p>
+    <p style="grid-column-start: 1; grid-row-start: 3; grid-column-end: 8; font-size: 1rem">
+      HTML CSS JAVASCRIPT REACT REDUX
+    </p>
+    <h5 style="grid-column-start: 1; grid-row-start: 4; grid-column-end: 8">
+      Built the login and display chart for
+      <span style="text-decoration: underline #4ee1a0">labs repository project.</span>
+    </h5>
+    <button
+      :style="{
+        color: store.colors.orochimaru,
+        margin: '.5rem 0',
+        fontFamily: 'spaceFontMed',
+        borderStyle: 'none',
+        backgroundColor: 'transparent',
+        paddingBottom: '0.3rem',
+        borderBottom: 'solid 2px #4ee1a0',
+        letterSpacing: '2px',
+        lineHeight: '26px',
+        gridColumnStart: '1',
+        gridRowStart: '5',
+        gridColumnEnd: '3'
+      }"
+    >
+      View Project
+    </button>
+    <button
+      :style="{
+        color: store.colors.orochimaru,
+        margin: '.5rem 0',
+        fontFamily: 'spaceFontMed',
+        borderStyle: 'none',
+        backgroundColor: 'transparent',
+        paddingBottom: '0.3rem',
+        borderBottom: 'solid 2px #4ee1a0',
+        letterSpacing: '2px',
+        lineHeight: '26px',
+        gridColumnStart: '4',
+        gridRowStart: '5',
+        gridColumnEnd: '7'
+      }"
+    >
+      <a
+        style="text-decoration: none; color: white"
+        href="https://github.com/alexJSanchez/asylum-rg-fe-starter"
         >View code</a
       >
     </button>
